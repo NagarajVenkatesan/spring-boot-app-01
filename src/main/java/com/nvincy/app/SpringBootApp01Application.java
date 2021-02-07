264c96e7f485a77d05c8441b101fd979b6e2d4f1
NagarajVenkatesan <nagaraj9325@gmail.com>
2021-02-07 14:17:02 +0530

    changed app to application

--- src/main/java/com/nvincy/app/SpringBootApp01Application.java
+++ src/main/java/com/nvincy/app/SpringBootApp01Application.java
@@ -8,7 +8,7 @@ public class SpringBootApp01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApp01Application.class, args);
-		System.out.println("Hello world from spring boot app");
+		System.out.println("Hello world from spring boot application");
 	}
 
 }
