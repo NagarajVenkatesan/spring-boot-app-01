1d16eaf3895193b4c6841d059305aab7c4c2a510
NagarajVenkatesan <37528451+NagarajVenkatesan@users.noreply.github.com>
2021-02-07 13:38:59 +0530

    added sop
    added sop

--- src/main/java/com/nvincy/app/SpringBootApp01Application.java
+++ src/main/java/com/nvincy/app/SpringBootApp01Application.java
@@ -8,6 +8,7 @@ public class SpringBootApp01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApp01Application.class, args);
+		System.out.println("Hello world from spring boot app");
 	}
 
 }
